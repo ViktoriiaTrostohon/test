4bcf91091ec218f518d4920c8fd5b4b2ef80e784
ViktoriiaTrostohon <84284942+ViktoriiaTrostohon@users.noreply.github.com>
2021-08-27 23:11:30 +0300

    js practice
    webinar 1

---
+++ js/script-1.js
@@ -0,0 +1,74 @@
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = 'Chelsy';
+// const message = 'Welcome';
+// const isOpen = true;
+// const shouldConfirm = false;
+
+// console.log('10')
+
+// const type = typeof 'Amazing';
+// console.log(type)
+
+// alert('why the fuck?')
+
+// const shouldRenew = confirm('Continue subscription?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('How much stuff do you want?');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+// let elementWidth = '200.74px';
+// elementWidth = Number.parseFloat(elementWidth);
+// console.log(elementWidth);
+
+// let salary = 1300.4567;
+// salary = Number(salary.toFixed(2));
+// console.log(salary);
+
+// const base = 3;
+// const power = 5;
+
+// const result = Math.pow(base, power);
+
+// console.log(result);
+
+// let base = prompt('Type a number');
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt('What power?');
+// power = Number(power);
+// console.log(power);
+
+// const result = base ** power;
+// console.log(result);
+
+// const message = 'Read this cool stuff';
+// console.log(message.length);
+
+// const firstName = 'Mark';
+// const lastName = 'Ethan';
+// const fullName = firstName + ' ' + lastName;
+// const room = 5;
+// const type = 'VIP';
+// console.log(`Guest ${fullName} is checking into ${type} room ${room}. Good luck to you, ${fullName}!`);
+
+// let brand = 'Samsung';
+// brand = brand.toUpperCase();
+// console.log(brand);
+
+// let brand = prompt('Gimme brand name');
+// brand = brand.toUpperCase();
+// console.log(brand);
+
+let brand = 'samsung';
+brand = brand[0].toUpperCase() + brand.slice(1).toLowerCase();
+console.log(brand);
+
